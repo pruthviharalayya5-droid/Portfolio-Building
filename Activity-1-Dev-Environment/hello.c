a175d063446c626216d003dd2e16113c517e451b
Prithvi H <pruthviharalayya5@gmail.com>
2026-09-03 09:52:08 +0530

    Add collaborative greeting function

--- Activity-1-Dev-Environment/hello.c
+++ Activity-1-Dev-Environment/hello.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
-int main()
-{
+
+void greet(const char *name){
+    printf("Hello,%s! welcome to our Github portfolio.\n",name);
+}
+
+int main(){
     printf("Hello world\n");
+
+    greet("Ada");
+
     return 0;
 }
